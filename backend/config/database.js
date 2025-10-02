ef7cc3c346de3cf56096b6cdbd57e36bbfb62a76
Carter <cartermar211@gmail.com>
2025-10-02 12:03:10 -0600

    Updated infrastructure for postgres migration

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,21 @@
 /**
- * Configures and exports the Sequelize instance used to connect to the SQLite database. 
+ * Configures and exports the Sequelize instance used to connect to the PostgreSQL database.
  * The Sequelize instance is imported into other parts of the application that need to interact with the database.
  */
 
-const { Sequelize } = require("sequelize");
-
-// Set up sqlite database connection
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "database.db",
-    // debugging
-    logging: console.log
-
-});
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+// Set up PostgreSQL database connection using environment variables
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: console.log,
+    }
+);
 
 module.exports = sequelize;
