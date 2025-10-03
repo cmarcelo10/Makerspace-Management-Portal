8aa092732b77594c5954529b24e66a3a3ed36f54
Carter <cartermar211@gmail.com>
2025-10-03 13:36:17 -0600

    [feat] enable SSL on backend database

--- backend/config/database.js
+++ backend/config/database.js
@@ -14,8 +14,13 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
         logging: console.log,
     }
 );
-
 module.exports = sequelize;
